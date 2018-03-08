acf38ef59563deee349e76c9e5007d6aded14ea5
Ozbey <unrealty@hotmail.com>
2018-03-08 15:47:24 +0100

    Edit create endpoint

--- apis/create.go
+++ apis/create.go
@@ -1,17 +1,87 @@
 package main
 
 import (
-    "fmt"
-    
+	"fmt"
+	"encoding/json"
+	"flag"
+	"os"
+	
     "github.com/aws/aws-lambda-go/events"
-    "github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+
+	"github.com/teris-io/shortid"
 )
 
+type Dog struct {
+	DogId string	 `json:"DogId"`
+    Name string      `json:"name"`
+    Age int          `json:"age"`
+	Weight float32   `json:"weight"`
+	Race string 	 `json:"race"`
+	FavFood string 	 `json:"favfood"`
+}
+
+type Config struct{
+	Region string
+	Table string
+}
+
+func (c *Config) Load() error {
+	flag.StringVar(&c.Table, "table", "Dogs", "Table to Query on")
+	flag.StringVar(&c.Region, "region", "us-west-1", "AWS Region the table is in")
+	flag.Parse()
+	if len(c.Table) == 0 {
+		flag.PrintDefaults()
+		return fmt.Errorf("table name is required.")
+	}
+	return nil
+}
+
+func exitWithError(err error) {
+	fmt.Fprintln(os.Stderr, err)
+	panic(err)
+}
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmt.Println("Received body: ", request.Body)
+	// Init dynamodb
+	config := &Config{}
+	if err:= config.Load(); err != nil{
+		exitWithError(err)
+	}
+	awsConfig := &aws.Config{}
+	awsConfig.WithRegion(config.Region)
+	sess := session.Must(session.NewSession(awsConfig))
+	dynamo := dynamodb.New(sess)
 
-	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
+	id, _ := shortid.Generate()
+	data := &Dog{
+		DogId: id,
+	}
+	json.Unmarshal([]byte(request.Body), data)
+	dynamoItem, _ := dynamodbattribute.MarshalMap(data)
+	fmt.Printf("dynamoItem %s", dynamoItem)
+	params := &dynamodb.PutItemInput{
+		Item: dynamoItem,
+		TableName: aws.String(config.Table),
+	}
+	_, errDb := dynamo.PutItem(params);
+	if errDb!= nil{
+		fmt.Printf("errDb %s", errDb.Error())
+		return events.APIGatewayProxyResponse{
+			Body: string(errDb.Error()),
+			StatusCode: 500,
+		}, nil
+	}else{
+		body, _ := json.Marshal(data)
+		return events.APIGatewayProxyResponse{
+			Body: string(body),
+			StatusCode: 200,
+		}, nil
+	}
 }
 
 func main() {
